4b5966b1069a970aa9fde7069997d12403b36f80
daggieblanqx <daggieblanqx@gmail.com>
2022-06-20 23:16:27 +0300

    Updated Code

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const main = async () => {
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
-    app.use(express.static(__dirname + '/public'));
     app.use('/', indexRoutes);
     app.use('*', (req, res) => res.status(404).send('404 Not Found'));
     app.listen(port, () =>
