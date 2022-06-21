633f944c6ef414407d2d2c71862ddc7510adaf1e
daggieblanqx <daggieblanqx@gmail.com>
2022-06-21 17:24:14 +0300

    Updated Code

--- utils/ecommerce_store.js
+++ utils/ecommerce_store.js
@@ -4,126 +4,55 @@ const PDFDocument = require('pdfkit');
 const fs = require('fs');
 
 module.exports = class EcommerceStore {
-    constructor() {
-        this.baseUrl = 'https://fakestoreapi.com';
+    constructor() {}
+    async _fetchAssistant(suffixUrl) {
+        return new Promise((resolve, reject) => {
+            let fullUrl = `https://fakestoreapi.com${suffixUrl}`;
+            request.get(fullUrl, (err, res, body) => {
+                try {
+                    if (err) {
+                        reject({
+                            status: 'failed',
+                            err,
+                        });
+                    }
+
+                    let data = JSON.parse(body);
+                    resolve({
+                        status: 'success',
+                        data,
+                    });
+                } catch (err) {
+                    reject({
+                        status: 'failed',
+                        error: err,
+                    });
+                }
+            });
+        });
     }
 
     async getProductById(productId) {
-        return new Promise((resolve, reject) => {
-            request.get(
-                `${this.baseUrl}/products/${productId}`,
-                (err, res, body) => {
-                    if (err) {
-                        reject({
-                            status: 'failed',
-                            err,
-                        });
-                    } else {
-                        try {
-                            let product = JSON.parse(body);
-
-                            let output = {
-                                status: 'success',
-                                data: product,
-                            };
-
-                            resolve(output);
-                        } catch (err) {
-                            reject({
-                                status: 'failed',
-                                err,
-                            });
-                        }
-                    }
-                }
-            );
-        });
+        return await this._fetchAssistant(`/products/${productId}`);
     }
     async getAllCategories() {
-        return new Promise((resolve, reject) => {
-            request.get(
-                `${this.baseUrl}/products/categories?limit=100`,
-                (err, res, body) => {
-                    if (err) {
-                        reject({
-                            status: 'failed',
-                            err,
-                        });
-                    } else {
-                        try {
-                            let categories = JSON.parse(body);
-                            // shuffle the categories
-                            categories = categories.sort(
-                                () => Math.random() - 0.5
-                            );
-                            // [1, 2, 3, 4].sort(() => (Math.random() > 0.5) ? 1 : -1)
-                            console.log({
-                                categories: categories.length,
-                            });
-                            resolve({
-                                status: 'success',
-                                data: categories,
-                            });
-                        } catch (err) {
-                            reject({
-                                status: 'failed',
-                                err,
-                            });
-                        }
-                    }
-                }
-            );
-        });
+        return await this._fetchAssistant('/products/categories?limit=100');
     }
     async getProductsInCategory(categoryId) {
-        return new Promise((resolve, reject) => {
-            request.get(
-                `${this.baseUrl}/products/category/${categoryId}?limit=10`,
-                (err, res, body) => {
-                    if (err) {
-                        reject({
-                            status: 'failed',
-                            err,
-                        });
-                    } else {
-                        try {
-                            let products = JSON.parse(body);
-                            // shuffle the products
-                            products = products.sort(() =>
-                                Math.random() > 0.5 ? 1 : -1
-                            );
-                            console.log({
-                                products: products.length,
-                            });
-                            let output = {
-                                status: 'success',
-                                data: products,
-                            };
-
-                            resolve(output);
-                        } catch (err) {
-                            reject({
-                                status: 'failed',
-                                err,
-                            });
-                        }
-                    }
-                }
-            );
-        });
+        return await this._fetchAssistant(
+            `/products/category/${categoryId}?limit=10`
+        );
     }
-    async generateInvoice({ order_details, file_path }) {
+
+    generateInvoice({ order_details, file_path }) {
         const doc = new PDFDocument();
         doc.pipe(fs.createWriteStream(file_path));
-        doc.fontSize(25).text(order_details, 100, 100);
+        doc.fontSize(25);
+        doc.text(order_details, 100, 100);
         doc.end();
-        return {
-            status: 'success',
-            data: {
-                file_path,
-            },
-        };
+        return;
     }
+
     generateRandomGeoLocation() {
         let storeLocations = [
             {
