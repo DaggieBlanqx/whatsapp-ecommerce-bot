3280e4be9bd56e9b56b4efd47a4f6e9eab340381
daggieblanqx <daggieblanqx@gmail.com>
2022-06-27 15:38:13 +0300

    Updated Code

---
+++ sample.env.js
@@ -0,0 +1,30 @@
+const production = {
+    ...process.env,
+    NODE_ENV: process.env.NODE_ENV || 'production',
+};
+
+const development = {
+    ...process.env,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    // PORT: '', 
+    // Meta_WA_VerifyToken: '',
+    // Meta_WA_accessToken: '',
+    // Meta_WA_SenderPhoneNumberId: '',
+    // Meta_WA_wabaId: '',
+};
+
+const fallback = {
+    ...process.env,
+    NODE_ENV: undefined,
+};
+
+module.exports = (environment) => {
+    console.log(`Execution environment selected is: "${environment}"`);
+    if (environment === 'production') {
+        return production;
+    } else if (environment === 'development') {
+        return development;
+    } else {
+        return fallback;
+    }
+};
